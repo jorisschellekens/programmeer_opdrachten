75516b6ee0a7eb7a5356ae223be1b3d05db7d7f7
jorisschellekens <joris.schellekens.1989@gmail.com>
2020-10-04 11:36:38 +0200

    andere eerste opdracht

--- opdracht_001.py
+++ opdracht_001.py
@@ -1,5 +1,11 @@
-# Write a Python program to get the Python version you are using
+# Schrijf een programma dat het gemiddelde van een lijst van getallen berekent
+from typing import List
+
+
+def gemiddelde(l: List[int]) -> float:
+    # TODO
+    return 0
 
 if __name__ == "__main__":
-    # TODO
-    pass
+    print("Het gemiddelde van [0, 10] is 5, jouw code zegt %f" % gemiddelde([0, 10]))
+    print("Het gemiddelde van [0, 1, 2, 3] is 1.5, jouw code zegt %f" % gemiddelde([0, 1, 2, 3]))
